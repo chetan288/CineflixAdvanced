eceb2536001333b52560e92e02b789f8b3d6ac20
chetan <chetan3211230@gmail.com>
2025-11-13 18:05:05 +0530

    intial project setup

---
+++ src/test/java/com/cineflix/Cineflix/CineflixApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cineflix.Cineflix;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CineflixApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
